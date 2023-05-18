ee8e189c27f38f0ac5870ef996f15b7ca053a450
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-05-18 21:20:03 +0200

    11 Logic for renting equipment

---
+++ src/main/java/pl/equipRental/assignment/AssignmentController.java
@@ -0,0 +1,35 @@
+package pl.equipRental.assignment;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.equipRental.exception.InvalidAssignmentException;
+
+import java.net.URI;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/assignments")
+public class AssignmentController {
+    private final AssignmentService assignmentService;
+
+
+
+    @PostMapping("")
+    public ResponseEntity<AssignmentDto> saveAssignment(@RequestBody AssignmentDto assignment) {
+        AssignmentDto savedAssignment;
+        try {
+            savedAssignment = assignmentService.createAssignment(assignment);
+        } catch(InvalidAssignmentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedAssignment.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(savedAssignment);
+    }
+}
